ee613f69bdec253722fd1a3c0d908dc3acbfcb85
Dotsworthy <dotsworthy@gmail.com>
2020-02-06 10:35:03 +0000

    added parts 3 & 4

---
+++ hogwarts/hogwarts_controller.rb
@@ -0,0 +1,17 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+
+require_relative("./models/student")
+also_reload("./models/*")
+
+
+get '/students' do
+  @students = Student.all()
+  erb(:index)
+end
+
+get '/students/:id' do
+  @student = Student.find(params[:id])
+  erb(:student)
+end
--- hogwarts/models/student.rb
+++ hogwarts/models/student.rb
@@ -2,38 +2,57 @@ require_relative('../db/sql_runner')
 
 class Student
 
-attr_reader  :first_name, :last_name, :age
-attr_accessor :house
+  attr_reader  :first_name, :last_name, :age
+  attr_accessor :house
 
-def initialize(options)
-  @id = options['id'].to_i
-  @first_name = options['first_name']
-  @last_name = options['last_name']
-  @house = options['house']
-  @age = options['age'].to_i
-end
+  def initialize(options)
+    @id = options['id'].to_i
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @house = options['house']
+    @age = options['age'].to_i
+  end
 
-def save()
-  sql = "INSERT INTO students
-  (
-    first_name,
-    last_name,
-    house,
-    age
-  )
-  VALUES
-  (
-    $1, $2, $3, $4
-  )
-  RETURNING id"
-  values = [@first_name, @last_name, @house, @age]
-  student_data = SqlRunner.run(sql, values)
-  @id = student_data.first()['id'].to_i
-end
+  def save()
+    sql = "INSERT INTO students
+    (
+      first_name,
+      last_name,
+      house,
+      age
+    )
+    VALUES
+    (
+      $1, $2, $3, $4
+    )
+    RETURNING id"
+    values = [@first_name, @last_name, @house, @age]
+    student_data = SqlRunner.run(sql, values)
+    @id = student_data.first()['id'].to_i
+  end
 
-def self.delete_all()
-  sql = "DELETE FROM students"
-  SqlRunner.run(sql)
-end
+  def pretty_name()
+    return "#{@first_name} #{@last_name}"
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM students"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM students"
+    students = SqlRunner.run (sql)
+    student = students.map{|student| Student.new(student)}
+    return student
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM students WHERE id = $1"
+    values = [id]
+    students = SqlRunner.run(sql, values)
+    student = Student.new (students.first)
+    return student
+  end
 
 end
